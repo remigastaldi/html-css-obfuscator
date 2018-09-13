1535d997b0d44c3f74a82c87e00203be01df27c1
hubert_i <leohub@live.fr>
2018-09-13 00:56:46 -0400

    First verssion

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-module.exports = function(source) {
+module.exports = function (source) {
 
+  const id = "data-v-" + Math.random().toString(36).substr(2, 9);
 
-  
+  let matchs = source.match(/(class=["']+[a-z.A-Z]+["'])/gm);
 
-    return "module.exports = " + JSON.stringify(source) + ";";
-  }
+  matchs.forEach(element => {
+    source = source.replace(element, element + ` ${id}`);
+  });
+
+  matchs = source.match(/[.]+[a-z+A-Z]+/gm);
+
+  matchs.forEach(element => {
+    source = source.replace(element, element + `[${id}] `);
+  });
+
+  return "module.exports = " + JSON.stringify(source) + ";";
+}
