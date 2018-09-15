ff5fd7e3c5fba7cf6c7610e97130e77c067e6a69
Remi Gastaldi <remi.gastaldi@epitech.eu>
2018-09-15 17:03:31 -0400

    regex algo optimisation

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
+const id = "data-v-" + Math.random().toString(36).substr(2, 9);
+
+function replaceCssClass(str, group1) {
+  return (group1  + `[${id}]`);
+}
+
+function replaceHtmlClass(str, group1) {
+  return (group1  + ` ${id}`);
+}
+
 module.exports = function (source) {
+  var html = source.replace(/(class=["']+[a-z0-9]+["'])/g, replaceHtmlClass);
+  var css = html.replace(/(\.[a-z0-9]+)(?={|:| |\.)/g, replaceCssClass);
 
-  const id = "data-v-" + Math.random().toString(36).substr(2, 9);
-
-  let matchs = source.match(/(class=["']+[a-z.A-Z]+["'])/gm);
-
-  matchs.forEach(element => {
-    source = source.replace(element, element + ` ${id}`);
-  });
-
-  matchs = source.match(/[.]+[a-z+A-Z]+/gm);
-
-  matchs.forEach(element => {
-    source = source.replace(element, element + `[${id}] `);
-  });
-
-  return "module.exports = " + JSON.stringify(source) + ";";
+  return "module.exports = " + JSON.stringify(css) + ';';
 }
